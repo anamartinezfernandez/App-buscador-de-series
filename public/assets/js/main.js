38c4b963c4c7dc87a6c2cf4c693e4a70afb8ec88
Ana Martinez Fernandez <anamartinezf6@gmail.com>
2020-10-23 09:02:37 +0200

    Add function connectToApi

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
-console.log('>> Ready :)');
+//DECLARACIÓN DE VARIABLES GLOBALES
+const btnSearch = document.querySelector (".js-searchBtn");
+const inputSearch = document.querySelector (".js-searchInput");
+const filmList = document.querySelector (".js-filmList");
+
+let filmsArrow = [];
+
+//CONECTAR CON EL SERVIDOR API
+
+
+function connectToApi(){
+  const inputSearchValue = inputSearch.value;
+  //console.log ("entro");
+  fetch (`http://api.tvmaze.com/search/shows?q=${inputSearchValue}`)
+  .then (function (response) {
+    console.log(response.json);
+    return response.json(); 
+  })
+  .then (function (data){
+    console.log(data);
+    filmsArrow = data; //me faltaba esto
+    paintCard();
+  })
+}
+
+
+
+btnSearch.addEventListener ("click", connectToApi);
 
 //# sourceMappingURL=main.js.map
