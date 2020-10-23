80cd1e72860b9d25b3dda822d5c1b1e48c547320
Ana Martinez Fernandez <anamartinezf6@gmail.com>
2020-10-23 14:14:55 +0200

    Add function js favouriteSeries

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,8 @@ const inputSearch = document.querySelector (".js-searchInput");
 const filmList = document.querySelector (".js-filmList");
 
 let filmsArrow = [];
-let favouriteSeries = [];
+let favouriteList = [];
+
 
 //CONECTAR CON EL SERVIDOR API cuando la usuaria hace click
 
@@ -36,19 +37,21 @@ const paintCard = function(){
   console.log (filmHtml);
 
   for (let i=0; i < filmsArrow.length; i++){
-    filmHtml += `<li class= "filmElement js-filmElement">`;
+    console.log (i);
+    filmHtml += `<li class= "filmElement js-filmElement" id ="${i}">`;
     console.log(filmsArrow[i].show);
     const films = filmsArrow[i].show;
     console.log (films);
     console.log (films.name);
+  
     if (films.image === null){
       filmHtml += `<img alt="foto carátula ${films.name}" name= "foto ${films.name}" src="https://via.placeholder.com/210x295/B695C0/525252/?text=tv">`;
     }
     else {
       
-      filmHtml += `<img alt="foto carátula ${films.name}" name= "foto ${films.name}" src="${films.image.medium}">`;
+      filmHtml += `<img alt="foto carátula ${films.name}" name= "foto ${films.name}" src="${films.image.medium}" >`;
     }
-    filmHtml += `<h3>${films.name}</h3>`;
+    filmHtml += `<h3 class= "filmTitle">${films.name}</h3>`;
     filmHtml += "</li>"
   }
   filmList.innerHTML = filmHtml;
@@ -58,25 +61,55 @@ const paintCard = function(){
 //ESCUCHAR TARJETAS DE VISITA
 
 function listenCard(){
-  console.log ("entro en funcióin listenCard");
+  console.log ("entro en función listenCard");
   const cardElements = document.querySelectorAll(".js-filmElement");
   console.log (cardElements);
-  for (const cardElement of cardElements) {
-    cardElement.addEventListener('click', favouritesFilms);
+  for (let cardElement of cardElements) {
+    cardElement.addEventListener('click', favouriteSeries);
     console.log (cardElement);
   }
 }
 
 //MARCAR COMO FAVORITAS LAS SERIRES SELECCIONADAS POR LA USUARIA (poner escuchador de eventos)
 
-const favouritesFilms = function(event){
+const favouriteSeries = function(event){
 //la escuchamos más arriba en la función listenCard
-  console.log ("entro en función FavouriteFillms");
+  console.log ("entro en función FavouriteSeries");
   console.log (event); //targetCurrent es null y target es la imagen
-  /* const clicked = event.targetCurrent; */
+
+  //Identifico el elemento clickado
+  const favouriteClicked = event.currentTarget;
+  console.log(favouriteClicked);
+  const favouriteClickedId = parseInt(event.currentTarget.id); 
+  console.log (favouriteClickedId );
+  const findFavourite = favouriteList.indexOf(favouriteClickedId);
+
+  //Guardo los favoritos en un array let favouriteList = [];
+    //esto significa que no está dentro del array
+  if (findFavourite === -1){
+    favouriteList.push(favouriteClickedId);
+    console.log (favouriteList);
+    favouriteClicked.classList.add ("favouriteSerie");
+
+  } else {
+    favouriteList.splice (findFavourite, 1); 
+      //aquí podríamos hacer una constante, 
+      //splice nos indica que si ya está en el array, no queremos volverlo a ponerlo como favorito, por lo que lo quitamos, solo ese.
+  }
+/* paintCard();
+listenCard(); */
+
+  // Busco el favorito en el array favouriteList:
+/*   console.log (cardElement);
+
+  const isFavourite = favouriteList.find (cardElement => cardElement.img.id === clicked); 
+  console.log(isFavourite); */
+
+  //le añado la clase de favoritos. se la damos desde css al js
+  //le quito la clase de favoritos
+
+
 }
-
-
 
 
 
