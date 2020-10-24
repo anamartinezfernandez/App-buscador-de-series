ddf6402cb3f5efae59417690ce528afd588efed8
Ana Martinez Fernandez <anamartinezf6@gmail.com>
2020-10-24 13:08:12 +0200

    Add function js getObject & update function js paintFavourites

---
+++ README.md
@@ -0,0 +1,39 @@
+- [x] Pintar las series cuando haga click en el botón (evento)
+  - [x] Escuchar el evento click en el botón
+  - [x] Fetch obtener datos del servidor (FUNCTION CONNECTTOAPI)
+  - [x] Pasear, datos,del servidor a JSON
+  - [x] Guardar las series en un array (seriesArrow)
+  - [x] Pintar la tarjeta en el HTML (usar datos dinámicos) con una imagen y el título (FUNCTION PAINTCARD)
+  - [x] Poner imagen de relleno cuando no hay imagen
+
+- [x] POnerlo bonito (con clases de css se las añado)
+
+- [ ] Marcar como favoritas, las seleccionada por la usuaria
+  - [x] Escuchar el evento click en cada tarjeta (FUNCTION LISTENCARD)
+  - [x] Identificar el elemento clicado (id con currentTarget)
+  - [x] Relaciónar el ID con array de favoritos
+  - [x] Guardar los favoritos en un Array (favouriteListArrow)
+  - [x] Añadir la clase de favorito (la traemos de css) para intercambiar color de fondo y fuente
+  - [x] Pintar tarjetas de favoritos: Mostrar un listado de las series favoritas.
+  - [x] Si vuelve a pinchar en la misma tarjeta no queremos que se añada al array (y por lo tanto tampoco a la lista)
+  - [ ] Con css que aparezca en la parte izquierda y hidden
+  - [x] Mantener las series favoritas aunque la usuaria realice otra búsqueda. ASEGURARSE DE POR QUÉ APARECE ASÍ
+
+- [] Localstorage: al recargar la página los favoritos deben de seguir mostrándose
+
+- [] Bonus: borrar favoritos
+
+- [] Bonus: Afinar la maquetación
+
+- [] Hacer README
+
+- [] Subir a docs. 
+
+
+Flujo de trabajo:
+
+- Evento escuchador de click (al final del código): conecta con servidor API
+- Función conectar con el servidor API cuando la usuaria hace click. (coge todos los datos del servidor y los guarda en el array seriesArrow)
+- La función api-->Activa la función PaintCard. 
+  - Se pinta la imagen si no la hay.
+- La función api-->Activa la función ListenCard (escuchar los favoritos)
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,7 +7,7 @@ const serieList = document.querySelector (".js-serieList");
 const favouriteSerieList  = document.querySelector (".js-favouriteSerieList");
 
 let seriesArrow = []; 
-let favouriteList = [];
+let favouriteListArrow = [];
 
 
 //CONECTAR CON EL SERVIDOR API cuando la usuaria hace click
@@ -15,7 +15,7 @@ let favouriteList = [];
 
 function connectToApi(event){
   const inputSearchValue = inputSearch.value;
-  //console.log ("entro");
+  console.log ("entro");
   fetch (`http://api.tvmaze.com/search/shows?q=${inputSearchValue}`)
   .then (function (response) {
     console.log(response.json);
@@ -35,19 +35,19 @@ event.preventDefault();
 //PINTAR TARJETAS DE SERIES
 
 const paintCard = function(){
-  console.log ("entro en paintcard");
+  //console.log ("entro en paintcard");
   let serieHtml = "";
-  console.log (serieHtml);
+  //console.log (serieHtml);
 
   for (let i=0; i < seriesArrow.length; i++){
-    console.log (i);
+    //console.log (i);
   
-    console.log(seriesArrow[i].show);
+    //console.log(seriesArrow[i].show);
     const series = seriesArrow[i].show;
-    console.log (series);
-    console.log (series.name);
-    console.log (seriesArrow);
-    console.log (series.id);
+    //console.log (series);
+    //console.log (series.name);
+    //console.log (seriesArrow);
+    //console.log (series.id);
     serieHtml += `<li class= "serieElement js-serieElement" id ="${series.id}">`;
   
     if (series.image === null){
@@ -61,18 +61,19 @@ const paintCard = function(){
     serieHtml += "</li>"
   }
   serieList.innerHTML = serieHtml;
-  console.log (serieList);
+  //console.log (serieList);
 }
+
 
 //ESCUCHAR TARJETAS DE VISITA
 
 function listenCard(){
-  console.log ("entro en función listenCard");
+  //console.log ("entro en función listenCard");
   const cardElements = document.querySelectorAll(".js-serieElement");
-  console.log (cardElements);
+ // console.log (cardElements);
   for (let cardElement of cardElements) {
     cardElement.addEventListener('click', favouriteSeries);
-    console.log (cardElement);
+    //console.log (cardElement);
   }
 }
 
@@ -81,7 +82,9 @@ function listenCard(){
 const getObject = function(id){
   console.log ("entro en getobject");
   console.log (seriesArrow);
-  console.log(seriesArrow.find(serieArrow => serieArrow.show.id === 41531));
+  console.log (favouriteListArrow);
+  console.log (seriesArrow.find(serieArrow => serieArrow.show.id === 41531));
+  console.log (seriesArrow.find(serieArrow => serieArrow.show.id === parseInt(id)));
  
   return seriesArrow.find(serieArrow => serieArrow.show.id === parseInt(id));
 
@@ -91,8 +94,8 @@ const getObject = function(id){
 
 const favouriteSeries = function(event){
 //la escuchamos más arriba en la función listenCard
-  console.log ("entro en función FavouriteSeries");
-  console.log (event); //targetCurrent es null y target es la imagen
+   //console.log ("entro en función FavouriteSeries");
+   //console.log (event); //targetCurrent es null y target es la imagen
 
   //Identifico el elemento clickado
   const favouriteClicked = event.currentTarget;
@@ -101,69 +104,58 @@ const favouriteSeries = function(event){
   console.log (favouriteClickedId);
   const object = getObject(favouriteClickedId);
   console.log(object);
-  const findFavourite = favouriteList.indexOf(favouriteClickedId);
+  
+  //const findFavourite = favouriteListArrow.indexOf(favouriteClickedId);
+  //console.log (findFavourite);
   
 
-  //Guardo los favoritos en un array let favouriteList = [];
+  
+  //Guardo los favoritos en un array let favouriteListArrow = [];
+  const findFavourite = favouriteListArrow.indexOf(object);
+  console.log (findFavourite);
+
     //esto significa que no está dentro del array
   if (findFavourite === -1){
-    favouriteList.push(favouriteClickedId); //¡¡¡dar una vuelta a lo que añadir aquí!!!1 Habría que relacionar de alguna forma el id de favoritos con el array de objetos 
-    console.log (favouriteList);
+    favouriteListArrow.push(object); //¡¡¡dar una vuelta a lo que añadir aquí!!!1 Habría que relacionar de alguna forma el id de favoritos con el array de objetos 
+    console.log(object);
+    console.log (favouriteListArrow);
     favouriteClicked.classList.add ("favouriteSerie");
     //pensar si hay que poner un remove
-    favouriteSerieList.innerHTML +=  `${object.show.name} ${object.show.url} `; //AQUÍ HAY QUE HACER QUE APAREZCA TOOOODO
-  /*   paintFavourites(); */
+    paintFavourites(); 
 
   } else {
-    favouriteList.splice (findFavourite, 1); 
-      //aquí podríamos hacer una constante, 
-      //splice nos indica que si ya está en el array, no queremos volverlo a ponerlo como favorito, por lo que lo quitamos, solo ese.
+    const findFavouriteLength = favouriteListArrow +1;
+    favouriteListArrow.splice(findFavouriteLength, 0); 
+    //Aquí lo que queremos es que si ya está en el array no se vuelva a añadir si se clicka. Por lo que usamos el método splice. El primer parámetro sería el índice a partir del cual queremos añadir elementos y el segundo el número de elementos que queremos añadir a partir de la posición dada. Para ello creamos una nueva variable (que cogeremos como primer parámetro) y que será la longitud del array +1. En el segundo parámetro le diremos que no queremos que nos añada nada más.
+    
+    console.log (favouriteListArrow);
   }
-/* paintCard();
-listenCard(); */
 }
+
 
 
 //PINTAR FAVORITAS EN ASIDE:
 
-/* const paintFavourites = function(){
-  favouriteList.innerHTML =  `${object}`;
-  
-} */
+const paintFavourites = function(){
+  let favouriteSerieHtml = "";
+  for (const favouriteObject of favouriteListArrow){
+    favouriteSerieHtml += "<li>";
+    if (favouriteObject.show.image === null){
+      favouriteSerieHtml += `<img alt="foto carátula ${favouriteObject.show.name}" name= "foto ${favouriteObject.show.name}" src="https://via.placeholder.com/210x295/B695C0/525252/?text=tv">`;
+    }
+    else {
+      
+      favouriteSerieHtml += `<img alt="foto carátula ${favouriteObject.show.name}" name= "foto ${favouriteObject.show.name}" src="${favouriteObject.show.image.medium}" >`;
+    }
+    favouriteSerieHtml += `<h3>${favouriteObject.show.name}</h3>`
+    favouriteSerieHtml += "</li>";
+  } 
+  favouriteSerieList.innerHTML = favouriteSerieHtml;
+
+}
 
 
-    //Hay que mostrar un listado en la parte izquierda de la pantalla, debajo del formulario de búsqueda, con
-/* las series favoritas. Os recomendamos crear un variable o constante de tipo array en JS para almacenar las series favoritas.
-Las series favoritas deben seguir apareciendo a la izquierda aunque la usuaria realice otra búsqueda. */
-
-
-
-
-//LOCAL STORAGE
-
-//2.1. Diagrama de flujo cuando la usuaria hace un evento.
-
-/* function setLocalStorage(){
-  localStorage.setItem ("textinStorage", favouriteSeries);
-  const stringFavouriteSeries = JSON.stringify (favouriteSeries);
-  }; */
-  
-//2.2. Diagrama de flujo al arrancar la página.
-
-  /* function getFromLocalStorage(){
-  const stringFavouriteStorage = localStorage.getItem ("textinStorage");
-  const favouriteStorage = JSON.parse (stringFavouriteStorage);
-
-  if (favouriteStorage !== null) {
- //aquí falta
-  }
-};
-
-
-favouriteSeries.addEventListener ("keyup", setLocalStorage); //aquí mirar el elemento sobre el que se pone el escuchador */
 
 btnSearch.addEventListener ("click", connectToApi);
 
-
-/* README */
 //# sourceMappingURL=main.js.map
